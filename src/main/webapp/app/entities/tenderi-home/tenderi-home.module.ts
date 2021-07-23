6c0a5d729767d81d56d33234d3a3e6131dfa6edd
drasko-kosovic <drasko.kosovic@gmail.com>
2021-07-23 16:40:58 +0200

    init

--- src/main/webapp/app/entities/tenderi-home/tenderi-home.module.ts
+++ src/main/webapp/app/entities/tenderi-home/tenderi-home.module.ts
@@ -7,9 +7,25 @@ import { TenderiHomeRoutingModule } from './route/tenderi-home-routing.module';
 import { MatTabsModule } from '@angular/material/tabs';
 import { SpecifikacijeModule } from '../specifikacije/specifikacije.module';
 import { JhMaterialModule } from '../../shared/jh-material.module';
+import { ViewPonudeModule } from '../view-ponude/view-ponude.module';
+import { ViewVrednovanjeModule } from '../view-vrednovanje/view-vrednovanje.module';
+import { PrvorangiraniModule } from '../prvorangirani/prvorangirani.module';
+import { HvalePonudeModule } from '../hvale-ponude/hvale-ponude.module';
+import { UgovorModule } from '../ugovor/ugovor.module';
 
 @NgModule({
-  imports: [SharedModule, TenderiHomeRoutingModule, MatTabsModule, SpecifikacijeModule, JhMaterialModule],
+  imports: [
+    SharedModule,
+    TenderiHomeRoutingModule,
+    MatTabsModule,
+    SpecifikacijeModule,
+    JhMaterialModule,
+    ViewPonudeModule,
+    ViewVrednovanjeModule,
+    PrvorangiraniModule,
+    HvalePonudeModule,
+    UgovorModule,
+  ],
   declarations: [TenderiHomeComponent, TenderiHomeDetailComponent],
 })
 export class TenderiHomeModule {}
--- src/main/webapp/app/login/login.component.ts
+++ src/main/webapp/app/login/login.component.ts
@@ -53,7 +53,7 @@ export class LoginComponent implements OnInit, AfterViewInit {
           this.authenticationError = false;
           if (!this.router.getCurrentNavigation()) {
             // There were no routing during login (eg from navigationToStoredUrl)
-            this.router.navigate(['']);
+            this.router.navigate(['postupci']);
           }
         },
         () => (this.authenticationError = true)
