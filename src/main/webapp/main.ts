6765c1a690e39ef009caa61be6eba99a04a81f35
drasko-kosovic <drasko.kosovic@gmail.com>
2021-07-23 18:22:55 +0200

    init

--- src/main/webapp/main.ts
+++ src/main/webapp/main.ts
@@ -1,4 +1,1 @@
-import { MatPaginatorIntl } from '@angular/material/paginator';
-import { getDutchPaginatorIntl } from './dutch-paginator-intl';
-
 import('./bootstrap').catch(err => console.error(err));
